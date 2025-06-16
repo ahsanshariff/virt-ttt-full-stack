8cea5f3e4240537e3a3261a199454a7b7f4784fd
ahsanshariff <158128436+ahsanshariff@users.noreply.github.com>
2025-06-16 23:48:06 +0530

    Update Ex.No.11.ts

--- Ex.No.11.ts
+++ Ex.No.11.ts
@@ -1,1 +1,6 @@
-
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+const original = "TypeScript";
+const reversed = reverseString(original);
+console.log(reversed);
